71c70670bd55bc11fa5cff011811ba46baa57d44
NurbakytOtegen <161034241+NurbakytOtegen@users.noreply.github.com>
2025-05-29 15:26:49 +0500

    Update environment.ts

--- Cars_Frontend-main/src/environments/environment.ts
+++ Cars_Frontend-main/src/environments/environment.ts
@@ -1,12 +1,5 @@
 // environment.ts (для разработки)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://nodejs-backend-mss3.onrender.com'
 };
-
-// environment.prod.ts (для продакшн-сборки)
-export const environment = {
-  production: true,
-  apiUrl: 'https://YOUR_BACKEND_SERVICE.onrender.com/api'
-};
-
